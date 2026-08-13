841132425d7778869a16f2de4157c46b82c23e85
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-08-13 13:08:22 +0200

    msixvc_new: add array and array_ref

--- crates/msixvc_new/src/parse/reader.rs
+++ crates/msixvc_new/src/parse/reader.rs
@@ -5,20 +5,47 @@ use super::BytesReader;
 use std::ops::Sub;
 
 use chrono::DateTime;
-use generic_array::{ArrayLength, GenericArray};
-use typenum::{Diff, U1, U2, U4, U8, U16};
+use generic_array::{ArrayLength, ConstArrayLength, GenericArray, IntoArrayLength};
+use typenum::{Const, Diff, U1, U2, U4, U8, U16};
 use uuid::Uuid;
 
-macro_rules! reader_method {
-    ($name:ident, $ret:ty, $n:ty, |$bytes:ident| $body:expr) => {
-        pub fn $name(self) -> ($ret, BytesReader<'a, Diff<Size, $n>>)
-        where
-            Size: Sub<$n, Output: ArrayLength>,
-        {
-            let (array, r) = self.advance::<$n>();
-            let $bytes = (*array).into();
+impl<'a, Size: ArrayLength> BytesReader<'a, Size> {
+    /// Gets a reference to an array containing the first `N` bytes of the reader.
+    pub fn array_ref<const N: usize>(
+        self,
+    ) -> (
+        &'a [u8; N],
+        BytesReader<'a, Diff<Size, ConstArrayLength<N>>>,
+    )
+    where
+        Const<N>: IntoArrayLength,
+        Size: Sub<ConstArrayLength<N>, Output: ArrayLength>,
+    {
+        let (head, reader) = self.advance();
+        (AsRef::<[u8; N]>::as_ref(head), reader)
+    }
 
-            ($body, r)
+    /// Gets an array containing the first `N` bytes of the reader.
+    pub fn array<const N: usize>(
+        self,
+    ) -> ([u8; N], BytesReader<'a, Diff<Size, ConstArrayLength<N>>>)
+    where
+        Const<N>: IntoArrayLength,
+        Size: Sub<ConstArrayLength<N>, Output: ArrayLength>,
+    {
+        let (head, reader) = self.advance();
+        (*AsRef::<[u8; N]>::as_ref(head), reader)
+    }
+}
+
+macro_rules! reader_method {
+    ($name:ident, $ret:ty, $n_ty:ty, $n:literal, |$bytes:ident| $body:expr) => {
+        pub fn $name(self) -> ($ret, BytesReader<'a, Diff<Size, $n_ty>>)
+        where
+            Size: Sub<$n_ty, Output: ArrayLength>,
+        {
+            let ($bytes, reader) = self.array::<$n>();
+            ($body, reader)
         }
     };
 }
@@ -37,19 +64,19 @@ const fn microsoft_filetime(filetime: i64) -> DateTime<chrono::Utc> {
 }
 
 impl<'a, Size: ArrayLength> BytesReader<'a, Size> {
-    reader_method!(u8_le, u8, U1, |b| u8::from_le_bytes(b));
-    reader_method!(i8_le, i8, U1, |b| i8::from_le_bytes(b));
-    reader_method!(u16_le, u16, U2, |b| u16::from_le_bytes(b));
-    reader_method!(i16_le, i16, U2, |b| i16::from_le_bytes(b));
-    reader_method!(u32_le, u32, U4, |b| u32::from_le_bytes(b));
-    reader_method!(i32_le, i32, U4, |b| i32::from_le_bytes(b));
-    reader_method!(u64_le, u64, U8, |b| u64::from_le_bytes(b));
-    reader_method!(i64_le, i64, U8, |b| i64::from_le_bytes(b));
+    reader_method!(u8_le, u8, U1, 1, |b| u8::from_le_bytes(b));
+    reader_method!(i8_le, i8, U1, 1, |b| i8::from_le_bytes(b));
+    reader_method!(u16_le, u16, U2, 2, |b| u16::from_le_bytes(b));
+    reader_method!(i16_le, i16, U2, 2, |b| i16::from_le_bytes(b));
+    reader_method!(u32_le, u32, U4, 4, |b| u32::from_le_bytes(b));
+    reader_method!(i32_le, i32, U4, 4, |b| i32::from_le_bytes(b));
+    reader_method!(u64_le, u64, U8, 8, |b| u64::from_le_bytes(b));
+    reader_method!(i64_le, i64, U8, 8, |b| i64::from_le_bytes(b));
 
-    reader_method!(uuid, Uuid, U16, |b| Uuid::from_bytes_le(b));
-    reader_method!(filetime, DateTime<chrono::Utc>, U8, |b| microsoft_filetime(
-        i64::from_le_bytes(b)
-    ));
+    reader_method!(uuid, Uuid, U16, 16, |b| Uuid::from_bytes_le(b));
+    reader_method!(filetime, DateTime<chrono::Utc>, U8, 8, |b| {
+        microsoft_filetime(i64::from_le_bytes(b))
+    });
 
     pub fn magic<N>(
         self,
